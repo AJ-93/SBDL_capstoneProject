38ea77d6ca21cf0e7ba34e605d57182c1216a560
AjayKumarChadayan <104778724+AjayKumarChadayan@users.noreply.github.com>
2025-04-13 21:40:06 +0200

    integrating with kafka

--- lib/Utils.py
+++ lib/Utils.py
@@ -3,6 +3,8 @@ import configparser
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
+KAFKA_JAR = "jars/spark-sql-kafka-0-10_2.12-3.4.4.jar"
+
 def get_spark_session(env):
     if env == "LOCAL":
         spark_conf = SparkConf()
@@ -16,6 +18,7 @@ def get_spark_session(env):
         return SparkSession.builder \
                 .config("spark.driver.extraJavaOptions",
                         "-Dlog4j.configuration=file:log4j.properties -Dspark.yarn.app.container.log.dir=app-logs -Dlogfile.name=sbdl-logs") \
+                .config("spark.jars.packages", KAFKA_JAR) \
                 .config(conf=spark_conf) \
                 .enableHiveSupport() \
                 .getOrCreate()
